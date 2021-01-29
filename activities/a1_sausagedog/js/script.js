e7ad438af957b446142a2a59d71f1de7ff53d9d0
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-01-28 20:50:42 -0500

    a1: activity start

---
+++ activities/a1_sausagedog/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
