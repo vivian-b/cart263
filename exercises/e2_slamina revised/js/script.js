6c7f9e76890f127e7f3fcacd67ab0b69c690eee5
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-02-04 23:08:09 -0500

    E2: testing 2

--- exercises/e2_slamina revised/js/script.js
+++ exercises/e2_slamina revised/js/script.js
@@ -7,145 +7,13 @@ Vivian Bui
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
-const animals =
+let agree =
 
     [
-      "aardvark",
-      "alligator",
-      "alpaca",
-      "antelope",
-      "ape",
-      "armadillo",
-      "baboon",
-      "badger",
-      "bat",
-      "bear",
-      "beaver",
-      "bison",
-      "boar",
-      "buffalo",
-      "bull",
-      "camel",
-      "canary",
-      "capybara",
-      "cat",
-      "chameleon",
-      "cheetah",
-      "chimpanzee",
-      "chinchilla",
-      "chipmunk",
-      "cougar",
-      "cow",
-      "coyote",
-      "crocodile",
-      "crow",
-      "deer",
-      "dingo",
-      "dog",
-      "donkey",
-      "dromedary",
-      "elephant",
-      "elk",
-      "ewe",
-      "ferret",
-      "finch",
-      "fish",
-      "fox",
-      "frog",
-      "gazelle",
-      "gila monster",
-      "giraffe",
-      "gnu",
-      "goat",
-      "gopher",
-      "gorilla",
-      "grizzly bear",
-      "ground hog",
-      "guinea pig",
-      "hamster",
-      "hedgehog",
-      "hippopotamus",
-      "hog",
-      "horse",
-      "hyena",
-      "ibex",
-      "iguana",
-      "impala",
-      "jackal",
-      "jaguar",
-      "kangaroo",
-      "koala",
-      "lamb",
-      "lemur",
-      "leopard",
-      "lion",
-      "lizard",
-      "llama",
-      "lynx",
-      "mandrill",
-      "marmoset",
-      "mink",
-      "mole",
-      "mongoose",
-      "monkey",
-      "moose",
-      "mountain goat",
-      "mouse",
-      "mule",
-      "muskrat",
-      "mustang",
-      "mynah bird",
-      "newt",
-      "ocelot",
-      "opossum",
-      "orangutan",
-      "oryx",
-      "otter",
-      "ox",
-      "panda",
-      "panther",
-      "parakeet",
-      "parrot",
-      "pig",
-      "platypus",
-      "polar bear",
-      "porcupine",
-      "porpoise",
-      "prairie dog",
-      "puma",
-      "rabbit",
-      "raccoon",
-      "ram",
-      "rat",
-      "reindeer",
-      "reptile",
-      "rhinoceros",
-      "salamander",
-      "seal",
-      "sheep",
-      "shrew",
-      "silver fox",
-      "skunk",
-      "sloth",
-      "snake",
-      "squirrel",
-      "tapir",
-      "tiger",
-      "toad",
-      "turtle",
-      "walrus",
-      "warthog",
-      "weasel",
-      "whale",
-      "wildcat",
-      "wolf",
-      "wolverine",
-      "wombat",
-      "woodchuck",
-      "yak",
-      "zebra"
+      "yes",
     ];
 
+let positive = false;
 /**
 Description of preload
 */
@@ -153,8 +21,9 @@ function preload() {
 
 }
 
-let currentAnimal =``;
+let currentQuestion =`Will you be my valentine`;
 let currentAnswer = ``;
+
 /**
 Description of setup
 */
@@ -164,7 +33,7 @@ function setup() {
 
   if (annyang) {
   let commands = {
-    'I think it is *animal': guessAnimal
+    'My answer is *agree': myAnswer
   };
   annyang.addCommands(commands);
   annyang.start();
@@ -183,38 +52,29 @@ Description of draw()
 function draw() {
   background(0);
 
-  if (currentAnswer === currentAnimal){
+  if (currentAnswer === `yes`){
     fill(0,255,0);
   }
   else{
     fill(255,0,0);
   }
 
-
 text(currentAnswer, width / 2, height / 2);
+fill(255,200,200);
+text(currentQuestion, width / 2, height / 3);
+
 }
 
 function mousePressed(){
-  currentAnimal = random(animals);
-  let reverseAnimal = reverseString(currentAnimal);
-  responsiveVoice.speak(reverseAnimal);
+ responsiveVoice.speak(currentQuestion, "UK English Male", {
+  pitch: 2,
+  rate: 1.5,
+  volume: 1
+});
 }
 
-function guessAnimal(animal){
+function myAnswer(agree){
 
-  currentAnswer = animal.toLowerCase();
+  currentAnswer = agree.toLowerCase();
 
-}
-/**
-Reverses the provided string
-*/
-function reverseString(string) {
-  // Split the string into an array of characters
-  let characters = string.split('');
-  // Reverse the array of characters
-  let reverseCharacters = characters.reverse();
-  // Join the array of characters back into a string
-  let result = reverseCharacters.join('');
-  // Return the result
-  return result;
 }
