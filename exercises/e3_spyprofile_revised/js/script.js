28ffe6fdb81f52e0eb7ba5bcf4be02b8dd885849
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-02-12 10:42:20 -0500

    e3: cleaning + commenting

--- exercises/e3_spyprofile_revised/js/script.js
+++ exercises/e3_spyprofile_revised/js/script.js
@@ -4,9 +4,11 @@
 Spy profile
 vivian bui
 
-randomized spy profile generator
+randomized spy profile generator with a password to revisit the profile.
+Incorrect password results in automatically reseting the profile.
 */
 
+// spy profile text variables
 let spyProfile = {
   name: ``,
   alias: ``,
@@ -14,13 +16,12 @@ let spyProfile = {
   password: ``,
 };
 
+// spy profile variables (data)
 let instrumentData = undefined;
 let objectData = undefined;
 let tarotData = undefined;
 
-/**
-Description of preload
-*/
+// data taken loaded from JSON
 function preload() {
 
   tarotData = loadJSON(`https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`);
@@ -29,55 +30,71 @@ function preload() {
 
 }
 
-
+// Prompt upon page landing (log in screen)
 function profileCheck() {
 
+  // check existing profile
   let data = JSON.parse(localStorage.getItem(`spy-profile-data`));
 
   // saved data (revisiting)
   if (data !== null) {
 
+    // message prompt
     let password = prompt(`Welcome back Agent, what is the password?  \n[WARNING!] Providing the wrong code will erase all previous data`);
+
+    // on successful login (correct password, matches)
     if (password === data.password) {
 
+      // reload corresponding data
       spyProfile.name = data.name;
       spyProfile.alias = data.alias;
       spyProfile.secretWeapon = data.secretWeapon;
       spyProfile.password = data.password;
 
     }
+
+    // on unsuccessful login (incorrect password, does not match)
     if (password !== data.password) {
 
-      generateSpyProfile('rebooting');
-      localStorage.removeItem('spy-profile-data');
-
-  }}
-    else {
+      // create new profile
       generateSpyProfile();
+
+      // delete previous profile
+      localStorage.removeItem('spy-profile-data');
     }
+  }
+
+  //  newcommer = new profile
+  else {
+    generateSpyProfile();
+  }
 }
 
-
+// create profile (name input from user and added random variables taken from JSON)
 function generateSpyProfile() {
+
+  // prompt message
   spyProfile.name = prompt(`Agent, state your name.`);
 
-  let instrument = random(instrumentData.instruments);
+  // variable: instrument
+  let instrument = random(instrumentData.instruments); //randomized data taken from JSON
+  spyProfile.alias = `The ${instrument}`; //display
 
-  spyProfile.alias = `The ${instrument}`;
+  // variable: secret weapon
+  spyProfile.secretWeapon = random(objectData.objects); //randomized data taken from JSON
 
-  spyProfile.secretWeapon = random(objectData.objects);
-
-  let card = random(tarotData.tarot_interpretations);
-
+  // variable: password
+  let card = random(tarotData.tarot_interpretations); //randomized data taken from JSON (+ specific selection)
   spyProfile.password = random(card.keywords);
 
   // Save the resulting profile to local storage
   localStorage.setItem(`spy-profile-data`, JSON.stringify(spyProfile));
-
 }
 
-function givenProfile() {
+// display profile
+function displayProfile() {
 
+  //  text (instructions + information/spy profile content)
   let profile = `** SPY PROFILE, DO NOT DISTRIBUTE. **
 
   Name: ${spyProfile.name}
@@ -85,14 +102,15 @@ function givenProfile() {
   SecretWeapon: ${spyProfile.secretWeapon}
   Password: ${spyProfile.password} `;
 
+  // display
   textFont(`Courier, monospace`);
   textSize(30);
   textAlign(LEFT, TOP);
   fill(255);
   text(profile, 100, 100);
-
 }
 
+// option to hide information
 function keyPressed() {
   if (keyCode === 27) {
 
@@ -101,41 +119,34 @@ function keyPressed() {
     spyProfile.secretWeapon = `*******`;
     spyProfile.password = `*******`;
   }
-
 }
 
-
-/**
-Description of setup
-*/
+// prompt pop up + canvas size
 function setup() {
   createCanvas(850, 600);
-
   profileCheck();
-
 }
 
-/**
-Description of draw()
-*/
+// background + text display(spy card information)
 function draw() {
   background(0);
-  backgroundOutline();
-  givenProfile();
-instructions();
+  addedContent();
+  displayProfile();
 }
 
-function instructions() {
+// background outline + text instruction display
+function addedContent() {
+
+  // white outline decoration
+  push();
+  noFill();
+  strokeWeight(3);
+  stroke(255);
+  rectMode(CENTER);
+  rect(width / 2, height / 2, 800, 500, 20);
+  pop();
+
+  // instruction text
   textSize(20);
   text(`press ESC to hide information`, 100, 500);
-}
-
-function backgroundOutline(){
-  push();
-noFill();
-strokeWeight(3);
-stroke(255);
-  rectMode(CENTER);
-  rect(width/2, height/2, 800, 500, 20);
-  pop();
 }
