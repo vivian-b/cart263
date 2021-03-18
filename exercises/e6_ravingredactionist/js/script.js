95958ccad278d113e733ef67fe3a1afb9d4eda74
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-03-18 17:17:45 -0400

    e7: clean up and commenting

--- exercises/e6_ravingredactionist/js/script.js
+++ exercises/e6_ravingredactionist/js/script.js
@@ -1,17 +1,21 @@
 'use strict';
 
 /**
-Title of Project
+E6: Raving Redactionist
 Vivian
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Reveal some text and erase the content
+
+> added blinking
+> added erase button (change text content and css)
+> stylize through css
 */
 
 'use strict';
 
+
 $(`.top-secret`).on(`click`, redact);
-setInterval(revelation, 200);
+setInterval(revelation, 800);
 
 
 function redact(event) {
@@ -23,44 +27,46 @@ function revelation() {
   $(`.redacted`).each(attemptReveal);
 }
 
-setInterval(attemptReveal, 20);
-
+//Random reveal/hide of some text
 function attemptReveal() {
   let r = Math.random();
-  if (r < 0.1) {
+  if (r < 0.1) { //randomized repeated action
     $(this).removeClass(`redacted`);
     $(this).addClass(`revealed`);
     $('.revealed').removeClass(`important`);
   }
 }
 
+//title and decoration slowly flickering
 function blinking() {
   $('.blink').fadeOut(500);
   $('.blink').fadeIn(500);
 }
-setInterval(blinking, 2200);
+setInterval(blinking, 2000); //timer
 
-setInterval(emphasize, 300);
 
+//hidden text randomly flickering red
 function emphasize() {
   let r = Math.random();
-  if (r < 0.2) {
+  if (r < 0.2) { //randomized repeated action
     $('.important').fadeIn(500);
     $('.revealed').addClass(`important`);
   }
 }
+setInterval(emphasize, 100); //timer
 
+//interacting with the written text changes the content
 $(`.important`).on(`click`, change);
-
 function change() {
-  $(this).text('Ipsum');
+  $(this).text('Ipsum'); //change text to "ipsum"
 }
 
+//interactions when pressing on the "erase" button
 $('button').click(function() {
-  $('p').css('color', 'black');
-  $('body').css('background-color', 'white');
-  $('p').text('[erased]');
-  $('button').remove();
-  $('.blink').removeClass(`blink`);
+  $('p').css('color', 'black'); //change text to black
+  $('body').css('background-color', 'white'); //change background to white
+  $('p').text('[erased]'); //change text content to [erased]
+  $('button').remove(); //delete the button
+  $('.blink').removeClass(`blink`); //stop blinking effect
 
 });
