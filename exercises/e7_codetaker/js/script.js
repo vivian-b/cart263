49c36265aac1416f31763575ff90c68a9b3c5bed
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-03-24 22:29:23 -0400

    e7: start

---
+++ exercises/e7_codetaker/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+CODE TAKER
+Vivian Bui
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
+
+$(`#answer`).droppable({
+  drop: function(event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter); //answer area
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`); //remove highlight
+    // Check if they got it
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  }
+});
