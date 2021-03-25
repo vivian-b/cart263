846774cbe2426d50e1887044bdf7f0e8959b5515
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-03-25 11:24:02 -0400

    e7: commenting and clean up

--- exercises/e7_codetaker/js/script.js
+++ exercises/e7_codetaker/js/script.js
@@ -4,13 +4,13 @@
 FOOD TAKER
 Vivian Bui
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Feed the black box with the appropriate words (food).
+Dialogue box appears once the word is dragged into the box.
 */
 
 "use strict";
 
-
+//pop up when the correct word(food) is dragged into the box
 $(`#solved-dialog`).dialog({
   autoOpen: false,
   buttons: {
@@ -20,6 +20,7 @@ $(`#solved-dialog`).dialog({
   }
 });
 
+//pop up when the wrong word(non-eddible) is dragged into the box
 $(`#wrong-dialog`).dialog({
   autoOpen: false,
   buttons: {
@@ -30,42 +31,43 @@ $(`#wrong-dialog`).dialog({
 });
 
 //hint appears once
+//green glow for food
 $(`.good`).one(`mouseover`, function(event) {
-  $(this).addClass(`want`, 500);
+  $(this).addClass(`want`, 500); //css class gives green color
   $(this).draggable()
 });
+//red glow for non-eddible object
+$(`.bad`).one(`mouseover`, function(event) {
+  $(this).addClass(`reject`, 500); //css class gives red color
+  $(this).draggable()
 
+});
+
+//hint fade out
 //hint disappears
 $(`.good`).one(`mouseout`, function(event) {
   $(this).removeClass(`want`, 500);
 });
-
-//hint appears once
-$(`.bad`).one(`mouseover`, function(event) {
-  $(this).addClass(`reject`, 500);
-  $(this).draggable()
-
-});
-
 //hint disappears
 $(`.bad`).one(`mouseout`, function(event) {
   $(this).removeClass(`reject`, 500);
 });
 
 
+//interaction between the word(answer) and the box
 $(`#answer`).droppable({
   drop: function(event, ui) {
     let word = ui.draggable.text();
-    $(this).append(word); //answer area
-    ui.draggable.draggable(`disable`);
+    $(this).append(word); //answer area adds current word that is being interacted with
+    ui.draggable.draggable(`disable`); //disable drag once inside
     ui.draggable.removeClass(`found`); //remove highlight
-    // Check if they got it
+    // Check if word dragged is food
     if (($(this).text() === `apples`)||($(this).text() === `pears`)||($(this).text() === `grapes`)||($(this).text() === `peaches`)){
       $(`#solved-dialog`).dialog(`open`);
       $(this).text(``); //answer area
     }
 
-
+    // Check if word dragged is non-eddible object
     if (($(this).text() === `shoe`)||($(this).text() === `poop`)||($(this).text() === `poison`)){
       $(`#wrong-dialog`).dialog(`open`);
       $(this).text(``); //answer area
