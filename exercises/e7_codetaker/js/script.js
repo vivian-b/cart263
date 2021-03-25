bb9066113efc0fbd285855817a5cb5ca811bb4d7
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-03-25 00:46:58 -0400

    e7: tests

--- exercises/e7_codetaker/js/script.js
+++ exercises/e7_codetaker/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**
-CODE TAKER
+FOOD TAKER
 Vivian Bui
 
 This is a template. You must fill in the title,
@@ -10,32 +10,65 @@ author, and this description to match your project!
 
 "use strict";
 
+
 $(`#solved-dialog`).dialog({
   autoOpen: false,
   buttons: {
-    "I know.": function() {
+    ":D": function() {
       $(this).dialog(`close`);
     }
   }
 });
 
-$(`.secret`).one(`mouseover`, function(event) {
-  $(this).addClass(`found`, 500);
-  $(this).draggable({
-    helper: `clone`
-  });
+$(`#wrong-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    ":c": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+//hint appears once
+$(`.good`).one(`mouseover`, function(event) {
+  $(this).addClass(`want`, 500);
+  $(this).draggable()
+});
+
+//hint disappears
+$(`.good`).one(`mouseout`, function(event) {
+  $(this).removeClass(`want`, 500);
+});
+
+//hint appears once
+$(`.bad`).one(`mouseover`, function(event) {
+  $(this).addClass(`reject`, 500);
+  $(this).draggable()
+
+});
+
+//hint disappears
+$(`.bad`).one(`mouseout`, function(event) {
+  $(this).removeClass(`reject`, 500);
 });
 
 
 $(`#answer`).droppable({
   drop: function(event, ui) {
-    let letter = ui.draggable.text();
-    $(this).append(letter); //answer area
+    let word = ui.draggable.text();
+    $(this).append(word); //answer area
     ui.draggable.draggable(`disable`);
     ui.draggable.removeClass(`found`); //remove highlight
     // Check if they got it
-    if ($(this).text() === `T`) {
+    if (($(this).text() === `apples`)||($(this).text() === `pears`)||($(this).text() === `grapes`)||($(this).text() === `peaches`)){
       $(`#solved-dialog`).dialog(`open`);
+      $(this).text(``); //answer area
+    }
+
+
+    if (($(this).text() === `shoe`)||($(this).text() === `poop`)||($(this).text() === `poison`)){
+      $(`#wrong-dialog`).dialog(`open`);
+      $(this).text(``); //answer area
     }
   }
 });
