c1eff1c272188cf178cf6669cd847130bf755648
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-04-01 02:05:05 -0400

    P2: link food and meter

--- projects/p2_simulation/js/script.js
+++ projects/p2_simulation/js/script.js
@@ -29,74 +29,90 @@ function setup() {
 Description of draw()
 */
 function draw() {
-background(255)
-  }
+  background(255)
+}
 
-$( function() {
-    $( "#tabs" ).tabs();
+$(function() {
+  $("#tabs").tabs();
 
-  } );
+});
 
-  $( function() {
-    $( "#number" )
-      .selectmenu()
-      .selectmenu( "menuWidget" )
-        .addClass( "overflow" );
-  } );
-
-let hungerBar = 50;
-let happyBar = 50;
-let cleanBar = 50;
-
-  $( function() {
-      $( ".progressbar1" ).progressbar({
-        value: hungerBar
-      });
-      $( ".progressbar2" ).progressbar({
-        value: happyBar
-      });
-      $( ".progressbar3" ).progressbar({
-        value: cleanBar
-      });
-    } );
+$(function() {
+  $("#number")
+    .selectmenu()
+    .selectmenu("menuWidget")
+    .addClass("overflow");
+});
 
 
-$( function() {
-   $( ".dialog" ).dialog({
-     autoOpen: false,
-     resizable: false,
-     height: "auto",
-     width: 400,
-     modal: true,
-     buttons: {
-       "Reset": function() {
-         $( this ).dialog( "close" );
-       },
-       Cancel: function() {
-         $( this ).dialog( "close" );
-       }
-     }
-   });
+$(function() {
+  $(".dialog").dialog({
+    autoOpen: false,
+    resizable: false,
+    height: "auto",
+    width: 400,
+    modal: true,
+    buttons: {
+      "Reset": function() {
+        $(this).dialog("close");
+      },
+      Cancel: function() {
+        $(this).dialog("close");
+      }
+    }
+  });
 
-   let button = document.getElementById(`btnReset`);
+  let button = document.getElementById(`btnReset`);
 
-   button.addEventListener(`click`, function(event) {
-     $( ".dialog" ).dialog( "open" );
+  button.addEventListener(`click`, function(event) {
+    $(".dialog").dialog("open");
 
-   });
- } );
+  });
+});
 
+let hungerBar;
 
-     let slider = document.getElementById(`slider`);
+  $(function() {
+let feedingFood = document.getElementById(`feedApple`);
 
-     // Listen for changes to the slider
-     slider.addEventListener(`change`, function(event) {
-       let value = event.target.value;
-     });
+feedingFood.addEventListener(`click`, function(event) {
+  hungerBar = 20;
+});
+});
 
+  $(function() {
+    $(".progressbar1").progressbar({
+      value: hungerBar
+    });
+    // $(".progressbar2").progressbar({
+    //   value: happyBar
+    // });
+    // $(".progressbar3").progressbar({
+    //   value: cleanBar
+    // });
+  });
+  //
+  // let happyBar = 50;
+  // let cleanBar = 50;
 
-              let feedingFood = document.getElementById(`feedApple`);
+// function actions(){
+//
+//   actionSummary = `aw`;
+//
+//   // Display the action recap in the match history
+//     $("#recap").append(`<p>${actionSummary}</p>`);
+//     // Adjust the overflow to see what was just added without scrolling
+//     updateScroll();
+// }
+//
+// function updateScroll() {
+//   // Scroll to the bottom of the recap div
+//   let log = document.getElementById("recap");
+//   log.scrollTop = log.scrollHeight;
+// }
 
-              feedingFood.addEventListener(`click`, function(event) {
-                hungerBar = 90;
-              });
+let slider = document.getElementById(`slider`);
+// Listen for changes to the slider
+slider.addEventListener(`change`, function(event) {
+  let value = event.target.value;
+});
