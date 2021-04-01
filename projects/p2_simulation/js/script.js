b2dfd69b4606a143c24a4b79350990cddaea9153
Vivian Bui <71355054+vivian-b@users.noreply.github.com>
2021-04-01 00:38:11 -0400

    P2: More UI (interactions)

--- projects/p2_simulation/js/script.js
+++ projects/p2_simulation/js/script.js
@@ -8,6 +8,7 @@ This is a template. You must fill in the title,
 author, and this description to match your project!
 */
 
+
 /**
 Description of preload
 */
@@ -61,3 +62,28 @@ $( function() {
     $( function() {
   $( "#slider" ).slider();
 } );
+
+let button = document.getElementById(`btnReset`);
+
+$( function() {
+   $( ".dialog" ).dialog({
+     autoOpen: false,
+     resizable: false,
+     height: "auto",
+     width: 400,
+     modal: true,
+     buttons: {
+       "Reset": function() {
+         $( this ).dialog( "close" );
+       },
+       Cancel: function() {
+         $( this ).dialog( "close" );
+       }
+     }
+   });
+
+   $( "#btnReset" ).on( "click", function() {
+     $( ".dialog" ).dialog( "open" );
+
+   });
+ } );
